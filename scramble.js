74a890d044d4aa4d0b22e1640459b46e05a969e7
VishalMadipadga <nbcuber@gmail.com>
2018-05-19 12:51:18 -0500

    Saved JS file
    Forgot to save original JS file

--- scramble.js
+++ scramble.js
@@ -1,39 +1,45 @@
-var instructions = [U, U2, U’, D, D2, D’, R, R2, R’, L, L2, L’, F, F2, F’, B, B2, B’];
-var scramble = [21];
- var instructionIndex;
-function hello()
-{
-	
-	document.writeln("hello");
-}
 function generateScramble()
 {
-   
-   for (int i =0; i < 21; i++)
+
+var instructions = ["U", "U2", "U’", "D", "D2", "D’", "R", "R2", "R’", "L", "L2", "L’", "F", "F2", "F’", "B", "B2", "B’"];
+var scramble = new Array(21);
+ var instructionIndex; 
+   for (var i =0; i < 21; i++)
    {
    instructionIndex = faceGenerator();
    instructionIndex = instructionIndex*3;
    var generate = Math.random()*(2-0)+0;
-   instructionIndex +=generate; 
+   var generateTrunc = generate | 0;
+   instructionIndex +=generateTrunc; 
    scramble[i] = instructions[instructionIndex];
    }
+   
+   printScramble(scramble);
    
 }
 
 
 function faceGenerator()
 {
-  return Math.random()* (5-0)+0;
-
+  var number =  Math.random()* (5-0)+0;
+  var truncated = number|0;
+  return truncated;
 
 }
-function printScramble()
+function printScramble(scramble)
 {
- for (int i =0; i < 21; i++)
+ var out = " ";
+ for (var i =0; i < 21; i++)
    {
-     
-   document.writeln(scramble[i]);
+     out += scramble[i];
+	 out += " ";
+     //document.getElementById("display").innerHTML += scramble[i];
+     /*var x = document.createElement("div");  // Creates a new <div> node
+     x.textContent = scramble[i];         // Sets the text content
+     document.body.appendChild(x);*/
+     //document.writeln(scramble[i]);
    
    }
+   document.getElementById("display").innerHTML = out;
 
 }
